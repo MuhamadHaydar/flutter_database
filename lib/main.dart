5aba547863f980439deffcc98e9559ea5dc057ae
Muhamad Andro <49116664+MuhamadHaydar@users.noreply.github.com>
2020-03-18 00:38:48 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:database()
+    );
+  }
+}
+
+class database extends StatefulWidget {
+  @override
+  _databaseState createState() => _databaseState();
+}
+
+class _databaseState extends State<database> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
