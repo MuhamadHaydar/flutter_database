7e03214bae6a76564c541605ad22125cf2beb538
Muhamad Andro <49116664+MuhamadHaydar@users.noreply.github.com>
2020-03-18 17:39:24 +0300

    Adding database get and initialize and onCreate in order to open database connections and create it. also add another dependency for document directory.

--- lib/databasehelper.dart
+++ lib/databasehelper.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
 
-
-class DatabaseHelper{
+class DatabaseHelper {
   static final _databaseName = 'Dog.db';
   static final _databaseVersion = 1;
 
@@ -23,4 +23,26 @@ class DatabaseHelper{
   // only have a single app-wide reference to the database.
   static Database _database;
 
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    // lazily instantiate the db the first time it is accessed
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  // this openss the database (and creates it if it does'n exist)
+  _initDatabase() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  //SQL code to create the database table.
+  Future _onCreate(Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE $dogTable( $columnId INTEGER PRIMARY KEY, $columnName TEXT NOT NULL, $columnAge INTEGER NOT NULL)');
+  }
 }
